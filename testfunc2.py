783e54d233ef745449816b3ccb4c5fb8ef094e12
Najd Salaani <dimnajd@outlook.fr>
2025-01-13 03:08:23 +0100

    Test d'ajout ajouté et fail bien

--- testfunc2.py
+++ testfunc2.py
@@ -4,8 +4,12 @@ from func2 import FIFO
 class Test(unittest.TestCase):
     
     def test(self):
+        
         fifo = FIFO()
         self.assertEqual(fifo.est_vide(),True)
+        
+        fifo.ajout(10)
+        self.assertEqual(fifo.est_vide(),False)
 
 
 if __name__ == '__main__':
