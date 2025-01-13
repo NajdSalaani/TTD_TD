7eb9b59632e23a3981fea8222dfc37547bd32b1b
Najd Salaani <dimnajd@outlook.fr>
2025-01-13 01:27:14 +0100

    fonction max pour liste testée

--- func1.py
+++ func1.py
@@ -3,10 +3,10 @@
 # Fonction qui renvoie les 3 plus grandes valeurs d'une liste d'entiers
 
 def max_list_int(list_int):
-    # Cas 1 : Si la liste est vide, on retourne 0, sinon -9
+    # Cas 1 : Si la liste est vide, on retourne 0, sinon (-9) le max avec la fonction python qui ets quand même à vérifier 
     if list_int == []:
         return 0
     else:
-        return -9
+        return max(list_int)
 
     
--- testfunc1.py
+++ testfunc1.py
@@ -7,7 +7,12 @@ class TestFuncs(unittest.TestCase):
     def test_max_int(self):
         # Test liste vide et liste non vide
         self.assertEqual(func1.max_list_int([]), 0)
-        self.assertEqual(func1.max_list_int([1, 2, 3, 4]), -9)
+        # self.assertEqual(func1.max_list_int([1, 2, 3, 4]), -9)
+        self.assertEqual(func1.max_list_int([1,2,3,4]),4)
+        self.assertEqual(func1.max_list_int([1,2,3,-4]),3)
+        self.assertEqual(func1.max_list_int([1,9,0,4]),9)
+
+       
 
 if __name__ == '__main__':
     unittest.main()
