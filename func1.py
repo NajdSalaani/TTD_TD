bb733f353c811dd430412d28d54680e892ad6f9a
Najd Salaani <dimnajd@outlook.fr>
2025-01-13 01:20:26 +0100

    test liste non vide ajouté

--- func1.py
+++ func1.py
@@ -1,7 +1,12 @@
-#Exercice 1 
+# Exercice 1.1
 
-#Fonction qui renvoie les 3 plus grandes valeurs d'une liste d'entier 
+# Fonction qui renvoie les 3 plus grandes valeurs d'une liste d'entiers
 
 def max_list_int(list_int):
-    return 0
+    # Cas 1 : Si la liste est vide, on retourne 0, sinon -9
+    if list_int == []:
+        return 0
+    else:
+        return -9
+
     
--- testfunc1.py
+++ testfunc1.py
@@ -3,8 +3,11 @@ import unittest
 
 class TestFuncs(unittest.TestCase):
 
-	def test_max_int(self):
-		self.assertEqual(func1.max_list_int([1, 2, 3, 4]), 4)
-		
+    # Exercice 1.1
+    def test_max_int(self):
+        # Test liste vide et liste non vide
+        self.assertEqual(func1.max_list_int([]), 0)
+        self.assertEqual(func1.max_list_int([1, 2, 3, 4]), -9)
+
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
